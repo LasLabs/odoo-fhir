67003c6f842cc6a1fff39d91bd6ca294133ec033
Luigi Sison <lsison@moxylus.com>
2016-07-06 17:57:07 -0700

    Activate Practitioner menu

--- addons/hc_practitioner/__openerp__.py
+++ addons/hc_practitioner/__openerp__.py
@@ -33,8 +33,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/hc_practitioner_views.xml',
+        'security/ir.model.access.csv',
+        'views/hc_practitioner_views.xml',
         'views/hc_practitioner_templates.xml',
     ],
     # only loaded in demonstration mode
