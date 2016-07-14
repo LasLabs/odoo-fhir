db347eaaeab1a283514de98b74f9cd750a272161
Luigi Sison <lsison@moxylus.com>
2016-07-14 13:34:29 -0700

    Update hc_annotation.py

--- addons/hc_base/models/hc_annotation.py
+++ addons/hc_base/models/hc_annotation.py
@@ -25,6 +25,20 @@ class Annotation(models.AbstractModel):
             ("patient", "Patient"),
             ("related person", "Related Person")],
         help="Type of individual responsible for the annotation.")
+    author_name = fields.Char(
+        string="Author Name",
+        compute="compute_author_name",
+        help="Individual responsible for the annotation.")
     # author_practitioner_id = fields.Many2one(comodel_name="hc.res.practitioner", string="Author Practitioner", help="Practitioner responsible for the annotation.")
     # author_patient_id = fields.Many2one(comodel_name="hc.res.patient", string="Author Patient", help="Patient responsible for the annotation.")
     # author_related_person_id = fields.Many2one(comodel_name="hc.res.related.person", string="Author Related Person", help="Related person responsible for the annotation.")
+
+    @api.multi
+    def compute_author_name(self):
+        for hc_annot in self:
+            if hc_annot.author_type == 'practitioner':
+                hc_annot.author_name = hc_annot.author_practitioner_id.name
+            elif hc_annot.author_type == 'patient':
+                hc_annot.author_name = hc_annot.author_patient_id.name
+            elif hc_annot.author_type == 'related person':
+                hc_annot.author_name = hc_annot.author_related_person_id.name
