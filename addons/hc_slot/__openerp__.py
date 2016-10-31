eaa2f44d72bd87ffcc68551d3a9a5d87e5871acd
Luigi Sison <lsison@moxylus.com>
2016-10-31 08:30:37 -0700

    changes

--- addons/hc_slot/__openerp__.py
+++ addons/hc_slot/__openerp__.py
@@ -1,35 +1,47 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hc_slot",
+    'name': "Slot",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Time slot
+        """,
 
     'description': """
-        Long description of module's purpose
+        A slot of time on a schedule that may be available for booking appointments.
+
+        **Scope and Usage** 
+        
+        Slot resources are used to provide time-slots that can be booked using an appointment. They do not provide any information about appointments that are available, 
+        just the time, and optionally what the time can be used for. These are effectively spaces of free/busy time.
+        
+        Slots can also be marked as busy without having appointments associated.
+
+        A slot can have more than one appointment allocated to it. A scheduling system may permit multiple allocations up to a specific number of places. 
+        An example of this type of usage could be where the slot is being used for a group service that permits 5 participants at the same time.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Luigi Sison",
+    'website': "http://build.fhir.org/slot.html",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Health Care',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['hc_schedule'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/hc_res_slot_views.xml',
+        'views/hc_res_slot_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': 'True',
+    'auto-install': 'True',
 }
--- addons/hc_slot/models/hc_res_slot.py
+++ addons/hc_slot/models/hc_res_slot.py
@@ -2,14 +2,67 @@
 
 from openerp import models, fields, api
 
-# class hc_slot(models.Model):
-#     _name = 'hc_slot.hc_slot'
+class Slot(models.Model):    
+    _name = "hc.res.slot"    
+    _description = "Slot"        
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    identifier_ids = fields.One2many(
+        comodel_name="hc.slot.identifier", 
+        inverse_name="slot_id", 
+        string="Identifiers", 
+        help="External Ids for this item.")                
+    service_category_id = fields.Many2one(
+        comodel_name="hc.vs.service.category", 
+        string="Service Category", 
+        help="A broad categorisation of the service that is to be performed during this appointment.")                
+    service_type_ids = fields.Many2many(
+        comodel_name="hc.vs.service.type", 
+        string="Service Types", 
+        help="The type of appointments that can be booked into this slot (ideally this would be an identifiable service - which is at a location, rather than the location itself). If provided then this overrides the value provided on the availability resource.")                
+    specialty_ids = fields.Many2many(
+        comodel_name="hc.vs.c80.practice.code", 
+        string="Specialties", 
+        help="The specialty of a practitioner that would be required to perform the service requested in this appointment.")                
+    type_id = fields.Many2one(
+        comodel_name="hc.vs.v2.appointment.reason.code", 
+        string="Type", 
+        help="The style of appointment or patient that has been booked in the slot (not service type).")                
+    schedule_id = fields.Many2one(
+        comodel_name="hc.res.schedule", 
+        string="Schedule", 
+        required="True", 
+        help="The schedule resource that this slot defines an interval of status information.")                
+    status = fields.Selection(
+        string="Slot Status", 
+        required="True", 
+        selection=[("busy", "Busy"), ("free", "Free"), ("busy-unavailable", "Busy-Unavailable"), ("busy-tentative", "Busy-Tentative"), ("entered-in-error", "Entered-In-Error")], 
+        help="The free/busy status of the slot.")                
+    start = fields.Datetime(
+        string="Start Date", 
+        required="True", 
+        help="Date/Time that the slot is to begin.")                
+    end = fields.Datetime(
+        string="End Date", 
+        required="True", 
+        help="Date/Time that the slot is to conclude.")                
+    is_overbooked = fields.Boolean(
+        string="Overbooked", 
+        help="This slot has already been overbooked, appointments are unlikely to be accepted for this time.")                
+    comment = fields.Text(
+        string="Comment", 
+        help="Comments on the slot to describe any extended information. Such as custom constraints on the slot.")                
+
+class SlotIdentifier(models.Model):    
+    _name = "hc.slot.identifier"    
+    _description = "Slot Identifier"        
+    _inherit = ["hc.basic.association", "hc.identifier"]
+
+    slot_id = fields.Many2one(
+        comodel_name="hc.res.slot", 
+        string="Slot", 
+        help="Slot associated with this Slot Identifier.")                
+
+class V2AppointmentReasonCode(models.Model):    
+    _name = "hc.vs.v2.appointment.reason.code"  
+    _description = "V2 Appointment Reason Code"     
+    _inherit = ["hc.value.set.contains"]
