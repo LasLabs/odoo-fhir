0d7d259c8ffe5bd624c03774c3eaab7ea17f7640
Luigi Sison <lsison@moxylus.com>
2016-10-31 09:31:51 -0700

    changes

--- addons/hc_appointment/__openerp__.py
+++ addons/hc_appointment/__openerp__.py
@@ -1,35 +1,47 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hc_appointment",
+    'name': "Appointment",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Scheduled events (e.g., clinical visit, scheduled surgery, device reservation)
+        """,
 
     'description': """
-        Long description of module's purpose
+        A booking of a healthcare event among patient(s), practitioner(s), related person(s) and/or device(s) for a specific date/time. This may result in one or more Encounter(s).
+
+        **Scope and Usage** 
+        
+        Appointment resources are used to provide information about a planned meeting that may be in the future or past. The resource only describes a single meeting, 
+        a series of repeating visits would require multiple appointment resources be created for each instance. Examples include a scheduled surgery, 
+        a follow-up for a clinical visit, a scheduled conference call between clinicians to discuss a case, the reservation of a piece of diagnostic equipment 
+        for a particular use, etc. The visit scheduled by an appointment may be in person or remote (by phone, video conference, etc.) All that matters is that the time and usage of one or more individuals, locations and/or pieces of equipment is being fully or partially reserved for a designated period of time.
+
+        This definition takes the concepts of appointments in a clinical setting and also extends them to be relevant in the community healthcare space, 
+        and also ease exposure to other appointment / calendar standards widely used outside of healthcare.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Luigi Sison",
+    'website': "http://build.fhir.org/appointment.html",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Health Care',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['hc_slot'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/hc_res_appointment_views.xml',
+        'views/hc_res_appointment_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': 'True',
+    'auto-install': 'True',
 }
--- addons/hc_appointment/models/hc_res_appointment.py
+++ addons/hc_appointment/models/hc_res_appointment.py
@@ -2,14 +2,191 @@
 
 from openerp import models, fields, api
 
-# class hc_appointment(models.Model):
-#     _name = 'hc_appointment.hc_appointment'
+class Appointment(models.Model):    
+    _name = "hc.res.appointment"    
+    _description = "Appointment"        
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    identifier_ids = fields.One2many(
+        comodel_name="hc.appointment.identifier", 
+        inverse_name="appointment_id", 
+        string="Identifiers", 
+        help="External Ids for this item.")                
+    status = fields.Selection(
+        string="Appointment Status", 
+        required="True", 
+        selection=[
+            ("proposed", "Proposed"), 
+            ("pending", "Pending"), 
+            ("booked", "Booked"), 
+            ("arrived", "Arrived"), 
+            ("fulfilled", "Fulfilled"), 
+            ("cancelled", "Cancelled"), 
+            ("noshow", "No Show")], 
+        help="The overall status of the Appointment.")                
+    service_category_id = fields.Many2one(
+        comodel_name="hc.vs.service.category", 
+        string="Appointment Service Category", 
+        help="A broad categorisation of the service that is to be performed during this appointment.")              
+    service_type_ids = fields.Many2many(
+        comodel_name="hc.vs.service.type", 
+        string="Service Types", 
+        help="The specific service that is to be performed during this appointment.")                
+    specialty_ids = fields.Many2many(
+        comodel_name="hc.vs.c80.practice.code", 
+        string="Specialties", 
+        help="The specialty of a practitioner that would be required to perform the service requested in this appointment.")                
+    appointment_type_id = fields.Many2one(
+        comodel_name="hc.vs.v2.appointment.reason.code", 
+        string="Appointment Type", 
+        help="The style of appointment or patient that has been booked in the slot (not service type).")                
+    reason_id = fields.Many2one(
+        comodel_name="hc.vs.encounter.reason", 
+        string="Reason", 
+        help="The reason that this appointment is being scheduled, this is more clinical than administrative.")                
+    priority = fields.Integer(
+        string="Priority", 
+        help="The priority of the appointment. Can be used to make informed decisions if needing to re-prioritize appointments. (The iCal Standard specifies 0 as undefined, 1 as highest, 9 as lowest priority).")                
+    description = fields.Char(
+        string="Description", 
+        help="The brief description of the appointment as would be shown on a subject line in a meeting request, or appointment list. Detailed or expanded information should be put in the comment field.")                
+    start = fields.Datetime(
+        string="Start Date", 
+        required="True", 
+        help="Date/Time that the appointment is to take place.")                
+    end = fields.Datetime(
+        string="End Date", 
+        required="True", 
+        help="Date/Time that the appointment is to conclude.")
+    minutes_duration = fields.Integer(
+        string="Minutes Duration", 
+        help="Can be less than start/end (e.g. estimate).")                             
+    slot_ids = fields.One2many(
+        comodel_name="hc.appointment.slot", 
+        inverse_name="appointment_id", 
+        string="Slots", 
+        help="The slot that this appointment is filling. If provided then the schedule will not be provided as slots are not recursive, and the start/end values MUST be the same as from the slot.")                
+    created = fields.Datetime(
+        string="Appointment Creation Date", 
+        help="The date that this appointment was initially created.")                
+    comment = fields.Char(
+        string="Comment", 
+        help="Additional comments about the appointment.")                
+    requested_period_ids = fields.One2many(
+        comodel_name="hc.appointment.requested.period", 
+        inverse_name="appointment_id", 
+        string="Requested Periods", 
+        help="The slot that this appointment is filling. If provided then the schedule will not be provided as slots are not recursive, and the start/end values MUST be the same as from the slot.")                               
+    participant_ids = fields.One2many(
+        comodel_name="hc.appointment.participant", 
+        inverse_name="appointment_id", 
+        string="Participants", 
+        required="True", 
+        help="List of participants involved in the appointment.")                
+
+class AppointmentParticipant(models.Model):    
+    _name = "hc.appointment.participant"    
+    _description = "Appointment Participant"        
+    
+    appointment_id = fields.Many2one(
+        comodel_name="hc.res.appointment", 
+        string="Appointment", 
+        help="Appointment associated with this Appointment Participant.")                
+    type_ids = fields.Many2many(
+        comodel_name="hc.vs.encounter.participant.type", 
+        string="Types", 
+        help="Role of participant in the appointment.")                
+    actor_type = fields.Selection(
+        string="Actor Type", 
+        selection=[
+            ("Patient", "Patient"), 
+            ("Practitioner", "Practitioner"), 
+            ("Related Person", "Related Person"), 
+            ("Device", "Device"), 
+            ("Healthcare Service", "Healthcare Service"), 
+            ("Location", "Location")], 
+        help="Type of what is account tied to.")                
+    actor_name = fields.Char(
+        string="Actor", 
+        compute="_compute_actor_name", 
+        store="True", 
+        help="A Person, Location, Healthcare Service or Device that is participating in the appointment.")                
+    actor_patient_id = fields.Many2one(
+        comodel_name="hc.res.patient", 
+        string="Actor Patient", 
+        help="Patient that is participating in the appointment.")                
+    actor_practitioner_id = fields.Many2one(
+        comodel_name="hc.res.practitioner", 
+        string="Actor Practitioner", 
+        help="Practitioner that is participating in the appointment.")                
+    actor_related_person_id = fields.Many2one(
+        comodel_name="hc.res.related.person", 
+        string="Actor Related Person", 
+        help="Related Person that is participating in the appointment.")                
+    actor_device_id = fields.Many2one(
+        comodel_name="hc.res.device", 
+        string="Actor Device", 
+        help="Device that is participating in the appointment.")                
+    actor_healthcare_service_id = fields.Many2one(
+        comodel_name="hc.res.healthcare.service", 
+        string="Actor Healthcare Service", 
+        help="Healthcare Service that is participating in the appointment.")                
+    actor_location_id = fields.Many2one(
+        comodel_name="hc.res.location", 
+        string="Actor Location", 
+        help="Location that is participating in the appointment.")                
+    required = fields.Selection(
+        string="Participant Required", 
+        selection=[
+            ("required", "Required"), 
+            ("optional", "Optional"), 
+            ("information-only", "Information-Only")], 
+        help="Is this participant required to be present at the meeting.")                
+    status = fields.Selection(
+        string="Participant Status", 
+        required="True", 
+        selection=[
+            ("accepted", "Accepted"), 
+            ("declined", "Declined"), 
+            ("tentative", "Tentative"), 
+            ("needs-action", "Needs-Action")], 
+        help="Participation status of the Patient.")                
+
+class AppointmentIdentifier(models.Model):    
+    _name = "hc.appointment.identifier"    
+    _description = "Appointment Identifier"        
+    _inherit = ["hc.basic.association", "hc.identifier"]
+
+    appointment_id = fields.Many2one(
+        comodel_name="hc.res.appointment", 
+        string="Appointment", 
+        help="Appointment associated with this Appointment Identifier.")                
+
+class AppointmentSlot(models.Model):    
+    _name = "hc.appointment.slot"    
+    _description = "Appointment Slot"        
+    _inherit = ["hc.basic.association"]
+
+    appointment_id = fields.Many2one(
+        comodel_name="hc.res.appointment", 
+        string="Appointment", 
+        help="Appointment associated with this Appointment Slot.")                
+    slot_id = fields.Many2one(
+        comodel_name="hc.res.slot", 
+        string="Slot", 
+        help="The slot that this appointment is filling. If provided then the schedule will not be provided as slots are not recursive, and the start/end values MUST be the same as from the slot")                
+
+class AppointmentRequestedPeriod(models.Model):    
+    _name = "hc.appointment.requested.period"    
+    _description = "Appointment Requested Period"        
+    _inherit = ["hc.basic.association"]
+
+    appointment_id = fields.Many2one(
+        comodel_name="hc.res.appointment", 
+        string="Appointment", 
+        help="Appointment associated with this Appointment Requested Period.")                
+    start_date = fields.Datetime(
+        string="Start Date", 
+        help="Start of the potential date/time interval(s) requested to allocate the appointment during.")                
+    end_date = fields.Datetime(
+        string="End Date", 
+        help="End of the potential date/time interval(s) requested to allocate the appointment during.")                
