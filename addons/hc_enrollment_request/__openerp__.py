ed259e4f01306d6072f508af56bbd5a4bceba729
Luigi Sison <lsison@moxylus.com>
2016-11-15 06:04:18 -0800

    changes

--- addons/hc_enrollment_request/__openerp__.py
+++ addons/hc_enrollment_request/__openerp__.py
@@ -25,11 +25,11 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['hc_base'],
+    'depends': ['hc_eligibility_request'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/hc_res_enrollment_request_views.xml',
         'views/hc_res_enrollment_request_templates.xml',  
     ],
--- addons/hc_enrollment_request/models/hc_res_enrollment_request.py
+++ addons/hc_enrollment_request/models/hc_res_enrollment_request.py
@@ -2,14 +2,73 @@
 
 from openerp import models, fields, api
 
-# class hc_enrollment_request(models.Model):
-#     _name = 'hc_enrollment_request.hc_enrollment_request'
+class EnrollmentRequest(models.Model):    
+    _name = "hc.res.enrollment.request"    
+    _description = "Enrollment Request"        
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    identifier_ids = fields.One2many(
+    	comodel_name="hc.enrollment.request.identifier", 
+    	inverse_name="enrollment_request_id", 
+    	string="Identifiers", 
+    	help="Business Identifier.")                
+    status = fields.Selection(
+    	string="Status", 
+    	selection=[
+    		("active", "Active"), 
+    		("suspended", "Suspended"), 
+    		("inactive", "Inactive"), 
+    		("entered in error", "Entered In Error")], 
+    	help="Indicates whether the care team is currently active, suspended, inactive, or entered in error.")                
+    ruleset_id = fields.Many2one(
+    	comodel_name="hc.vs.ruleset", 
+    	string="Ruleset", 
+    	help="Resource version.")                
+    original_ruleset_id = fields.Many2one(
+    	comodel_name="hc.vs.ruleset", 
+    	string="Original Ruleset", 
+    	help="Original version.")                
+    created = fields.Datetime(
+    	string="Enrollment Request Creation Date", 
+    	help="Creation date.")                
+    target_id = fields.Many2one(
+    	comodel_name="hc.res.organization", 
+    	string="Target", 
+    	help="Insurer.")                
+    provider_id = fields.Many2one(
+    	comodel_name="hc.res.practitioner", 
+    	string="Provider", 
+    	help="Responsible practitioner.")                
+    organization_id = fields.Many2one(
+    	comodel_name="hc.res.organization", 
+    	string="Organization", 
+    	help="Responsible organization.")                
+    subject_id = fields.Many2one(
+    	comodel_name="hc.res.patient", 
+    	string="Subject", 
+    	required="True", 
+    	help="The subject of the Products and Services.")                
+    coverage_id = fields.Many2one(
+    	comodel_name="hc.res.coverage", 
+    	string="Coverage", 
+    	required="True", 
+    	help="Insurance information.")                
+    relationship_id = fields.Many2one(
+    	comodel_name="hc.vs.enrollment.relationship", 
+    	string="Relationship", 
+    	required="True", 
+    	help="Patient relationship to subscriber.")                
+
+class EnrollmentRequestIdentifier(models.Model):    
+    _name = "hc.enrollment.request.identifier"    
+    _description = "Enrollment Request Identifier"        
+    _inherit = ["hc.basic.association", "hc.identifier"]
+
+    enrollment_request_id = fields.Many2one(
+    	comodel_name="hc.res.enrollment.request", 
+    	string="Enrollment Request", 
+    	help="Enrollment Request associated with this Enrollment Request Identifier.")                
+
+class EnrollmentRelationship(models.Model):    
+    _name = "hc.vs.enrollment.relationship"    
+    _description = "Enrollment Relationship"        
+    _inherit = ["hc.value.set.contains"]
