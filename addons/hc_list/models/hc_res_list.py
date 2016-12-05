96f198af6a61ec17f867556675adf29c3d44ee3c
Luigi Sison <lsison@moxylus.com>
2016-12-05 07:49:05 -0800

    changes

--- addons/hc_list/models/hc_res_list.py
+++ addons/hc_list/models/hc_res_list.py
@@ -6,41 +6,136 @@ class List(models.Model):
     _name = "hc.res.list"    
     _description = "List"        
 
-    identifier_ids = fields.One2many(comodel_name="hc.list.identifier", inverse_name="list_id", string="Identifiers", help="Business identifier.")                
-    status = fields.Selection(string="Status", required="True", selection=[("current", "Current"), ("retired", "Retired"), ("entered-in-error", "Entered-In-Error")], help="Indicates the current state of this list.")                
-    mode = fields.Selection(string="Mode", required="True", selection=[("working", "Working"), ("snapshot", "Snapshot"), ("changes", "Changes")], help="How this list was prepared.")                
-    title = fields.Text(string="Title", help="Descriptive name for the list.")                
-    code_id = fields.Many2one(comodel_name="hc.vs.list.code", string="Code", help="What the purpose of this list is.")                
-    subject_type = fields.Selection(string="Subject Type", selection=[("Patient", "Patient"), ("Group", "Group"), ("Device", "Device"), ("Location", "Location")], help="Type of resources that have the same subject.")                
-    subject_name = fields.Char(string="Subject", compute="_compute_subject_name", store="True", help="If all resources have the same subject.")                
-    subject_patient_id = fields.Many2one(comodel_name="hc.res.patient", string="Subject Patient", help="Patient same subject.")                
-    subject_group_id = fields.Many2one(comodel_name="hc.res.group", string="Subject Group", help="Group same subject.")                
-    subject_device_id = fields.Many2one(comodel_name="hc.res.device", string="Subject Device", help="Device same subject.")                
-    subject_location_id = fields.Many2one(comodel_name="hc.res.location", string="Subject Location", help="Location same subject.")                
-    encounter_id = fields.Many2one(comodel_name="hc.res.encounter", string="Encounter", help="Context in which list created.")                
-    list_date = fields.Datetime(string="List Date", help="When the list was prepared.")                
-    source_type = fields.Selection(string="Source Type", selection=[("Practitioner", "Practitioner"), ("Patient", "Patient"), ("Device", "Device")], help="Type of who and/or what defined the list contents.")                
-    source_name = fields.Char(string="Source", compute="_compute_source_name", store="True", help="Who and/or what defined the list contents.")                
-    source_practitioner_id = fields.Many2one(comodel_name="hc.res.practitioner", string="Source Practitioner", help="Practitioner who and/or what defined the list contents.")                
-    source_patient_id = fields.Many2one(comodel_name="hc.res.patient", string="Source Patient", help="Patient who and/or what defined the list contents.")                
-    source_device_id = fields.Many2one(comodel_name="hc.res.device", string="Source Device", help="Device who and/or what defined the list contents.")                
-    ordered_by_id = fields.Many2one(comodel_name="hc.vs.list.order", string="Ordered By", help="What order the list has.")                
-    note_ids = fields.One2many(comodel_name="hc.list.note", inverse_name="list_id", string="Notes", help="Comments about the list.")                
-    empty_reason_id = fields.Many2one(comodel_name="hc.vs.list.empty.reason", string="Empty Reason", help="Why list is empty.")                
-    entry_ids = fields.One2many(comodel_name="hc.list.entry", inverse_name="list_id", string="Entries", help="Entries in the list.")                
+    identifier_ids = fields.One2many(
+        comodel_name="hc.list.identifier", 
+        inverse_name="list_id", 
+        string="Identifiers", 
+        help="Business identifier.")                
+    status = fields.Selection(
+        string="Status", 
+        required="True", 
+        selection=[
+            ("current", "Current"), 
+            ("retired", "Retired"), 
+            ("entered-in-error", "Entered In Error")], 
+        help="Indicates the current state of this list.")                
+    mode = fields.Selection(
+        string="Mode", 
+        required="True", 
+        selection=[
+            ("working", "Working"), 
+            ("snapshot", "Snapshot"), 
+            ("changes", "Changes")], 
+        help="How this list was prepared.")                
+    title = fields.Text(
+        string="Title", 
+        help="Descriptive name for the list.")                
+    code_id = fields.Many2one(
+        comodel_name="hc.vs.list.code", 
+        string="Code", 
+        help="What the purpose of this list is.")                
+    subject_type = fields.Selection(
+        string="Subject Type", 
+        selection=[
+            ("Patient", "Patient"), 
+            ("Group", "Group"), 
+            ("Device", "Device"), 
+            ("Location", "Location")], 
+        help="Type of resources that have the same subject.")                
+    subject_name = fields.Char(
+        string="Subject", 
+        compute="_compute_subject_name", 
+        store="True", 
+        help="If all resources have the same subject.")                
+    subject_patient_id = fields.Many2one(
+        comodel_name="hc.res.patient", 
+        string="Subject Patient", 
+        help="Patient same subject.")                
+    subject_group_id = fields.Many2one(
+        comodel_name="hc.res.group", 
+        string="Subject Group", 
+        help="Group same subject.")                
+    subject_device_id = fields.Many2one(
+        comodel_name="hc.res.device", 
+        string="Subject Device", 
+        help="Device same subject.")                
+    subject_location_id = fields.Many2one(
+        comodel_name="hc.res.location", 
+        string="Subject Location", 
+        help="Location same subject.")                
+    encounter_id = fields.Many2one(
+        comodel_name="hc.res.encounter", 
+        string="Encounter", 
+        help="Context in which list created.")                
+    list_date = fields.Datetime(
+        string="List Date", 
+        help="When the list was prepared.")                
+    source_type = fields.Selection(
+        string="Source Type", 
+        selection=[
+            ("Practitioner", "Practitioner"), 
+            ("Patient", "Patient"), 
+            ("Device", "Device")], 
+        help="Type of who and/or what defined the list contents.")                
+    source_name = fields.Char(
+        string="Source", 
+        compute="_compute_source_name", 
+        store="True", 
+        help="Who and/or what defined the list contents.")                
+    source_practitioner_id = fields.Many2one(
+        comodel_name="hc.res.practitioner", 
+        string="Source Practitioner", 
+        help="Practitioner who and/or what defined the list contents.")                
+    source_patient_id = fields.Many2one(
+        comodel_name="hc.res.patient", 
+        string="Source Patient", 
+        help="Patient who and/or what defined the list contents.")                
+    source_device_id = fields.Many2one(
+        comodel_name="hc.res.device", 
+        string="Source Device", 
+        help="Device who and/or what defined the list contents.")                
+    ordered_by_id = fields.Many2one(
+        comodel_name="hc.vs.list.order", 
+        string="Ordered By", 
+        help="What order the list has.")                
+    note_ids = fields.One2many(
+        comodel_name="hc.list.note", 
+        inverse_name="list_id", 
+        string="Notes", 
+        help="Comments about the list.")                
+    empty_reason_id = fields.Many2one(
+        comodel_name="hc.vs.list.empty.reason", 
+        string="Empty Reason", 
+        help="Why list is empty.")                
+    entry_ids = fields.One2many(
+        comodel_name="hc.list.entry", 
+        inverse_name="list_id", 
+        string="Entries", 
+        help="Entries in the list.")                
 
 class ListEntry(models.Model):    
     _name = "hc.list.entry"    
     _description = "List Entry"        
 
-    list_id = fields.Many2one(comodel_name="hc.res.list", string="List", help="List associated with this List Entry.")                
-    flag_ids = fields.Many2many(comodel_name="hc.vs.list.entry.flag", string="Flags", help="Workflow information about this item.")                
-    is_deleted = fields.Boolean(string="Deleted", help="If this item is actually marked as deleted.")                
-    entry_date = fields.Datetime(string="Entry Date", help="When item added to list.")                
-    item_type = fields.Selection(string="Item Type", 
+    list_id = fields.Many2one(
+        comodel_name="hc.res.list", 
+        string="List", 
+        help="List associated with this List Entry.")                
+    flag_ids = fields.Many2many(
+        comodel_name="hc.vs.list.entry.flag", 
+        string="Flags", 
+        help="Workflow information about this item.")                
+    is_deleted = fields.Boolean(
+        string="Deleted", 
+        help="If this item is actually marked as deleted.")                
+    entry_date = fields.Datetime(
+        string="Entry Date", 
+        help="When item added to list.")                
+    item_type = fields.Selection(
+        string="Item Type", 
         selection=[
             ("Family Member History", "Family Member History"), 
-            # ("Medication Request", "Medication Request"), 
+            ("Medication Request", "Medication Request"), 
             ("Allergy Intolerance", "Allergy Intolerance"), 
             ("Condition", "Condition"), 
             ("Procedure", "Procedure")], 
@@ -52,11 +147,12 @@ class ListEntry(models.Model):
         help="Actual entry.")                
     item_family_member_history_id = fields.Many2one(
         comodel_name="hc.res.family.member.history", 
-        string="Item Family Member History", help="Family Member History actual entry.")                
-    # item_medication_request_id = fields.Many2one(
-    #     comodel_name="hc.res.medication.request", 
-    #     string="Item Medication Request", 
-    #     help="Medication Request actual entry.")                
+        string="Item Family Member History", 
+        help="Family Member History actual entry.")                
+    item_medication_request_id = fields.Many2one(
+        comodel_name="hc.res.medication.request", 
+        string="Item Medication Request", 
+        help="Medication Request actual entry.")                
     item_allergy_intolerance_id = fields.Many2one(
         comodel_name="hc.res.allergy.intolerance", 
         string="Item Allergy Intolerance", 
@@ -75,14 +171,20 @@ class ListIdentifier(models.Model):
     _description = "List Identifier"        
     _inherit = ["hc.basic.association", "hc.identifier"]
 
-    list_id = fields.Many2one(comodel_name="hc.res.list", string="List", help="List associated with this List Identifier.")                
+    list_id = fields.Many2one(
+        comodel_name="hc.res.list", 
+        string="List", 
+        help="List associated with this List Identifier.")                
 
 class ListNote(models.Model):    
     _name = "hc.list.note"    
     _description = "List Note"        
     _inherit = ["hc.basic.association", "hc.annotation"]
 
-    list_id = fields.Many2one(comodel_name="hc.res.list", string="List", help="List associated with this List Note.")                
+    list_id = fields.Many2one(
+        comodel_name="hc.res.list", 
+        string="List", 
+        help="List associated with this List Note.")                
 
 class ListCode(models.Model):    
     _name = "hc.vs.list.code"    
