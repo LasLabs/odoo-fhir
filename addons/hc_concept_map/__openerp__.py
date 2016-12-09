a8131f78b7b5516bea63509515e47bb45a34c8cd
Luigi Sison <lsison@moxylus.com>
2016-12-09 14:18:29 -0800

    changes

--- addons/hc_concept_map/__openerp__.py
+++ addons/hc_concept_map/__openerp__.py
@@ -1,35 +1,43 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hc_concept_map",
+    'name': "Concept Map",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Concept relationships
+        """,
 
     'description': """
-        Long description of module's purpose
+        A statement of relationships from one set of concepts to one or more other concepts - either code systems or data elements, or classes in class models. 
+
+        **Scope and Usage** 
+        
+        A concept map defines a mapping from a set of concepts defined in a code system to one or more concepts defined in other code systems. 
+        Mappings are one way - from the source to the destination. In many cases, the reverse mappings are valid, but this cannot be assumed to be the case. 
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Luigi Sison",
+    'website': "https://hl7-fhir.github.io/conceptmap.html",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Health Care',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    # 'depends': ['hc_value_set'],
+    'depends': ['hc_structure_definition'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/hc_res_concept_map_views.xml',
+        'views/hc_res_concept_map_templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': 'True',
+    'auto-install': 'True',
 }
