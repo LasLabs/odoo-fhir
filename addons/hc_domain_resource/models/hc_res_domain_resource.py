99dcb1706900993b121e1e87b29c6559c054931a
Luigi Sison <lsison@moxylus.com>
2017-03-08 18:39:44 -0800

    changes

--- addons/hc_domain_resource/models/hc_res_domain_resource.py
+++ addons/hc_domain_resource/models/hc_res_domain_resource.py
@@ -6,10 +6,25 @@ class DomainResource(models.Model):
     _name = "hc.res.domain.resource"    
     _description = "Domain Resource"            
 
-    text_id = fields.Many2one(comodel_name="hc.domain.resource.text", string="Text", help="Text summary of the resource, for human interpretation.")                    
-    contained_ids = fields.One2many(comodel_name="hc.domain.resource.contained", inverse_name="domain_resource_id", string="Contained Resources", help="Contained, inline Resources.")                  
-    extension_ids = fields.One2many(comodel_name="hc.domain.resource.extension", inverse_name="domain_resource_id", string="Extensions", help="Additional Content defined by implementations.")                 
-    modifier_extension_ids = fields.One2many(comodel_name="hc.domain.resource.modifier.extension", inverse_name="domain_resource_id", string="Modifier Extensions", help="Extensions that cannot be ignored.")                  
+    text_id = fields.Many2one(
+        comodel_name="hc.domain.resource.text", 
+        string="Text", 
+        help="Text summary of the resource, for human interpretation.")                    
+    contained_ids = fields.One2many(
+        comodel_name="hc.domain.resource.contained", 
+        inverse_name="domain_resource_id", 
+        string="Contained Resources", 
+        help="Contained, inline Resources.")                  
+    extension_ids = fields.One2many(
+        comodel_name="hc.domain.resource.extension", 
+        inverse_name="domain_resource_id", 
+        string="Extensions", 
+        help="Additional Content defined by implementations.")                 
+    modifier_extension_ids = fields.One2many(
+        comodel_name="hc.domain.resource.modifier.extension", 
+        inverse_name="domain_resource_id", 
+        string="Modifier Extensions", 
+        help="Extensions that cannot be ignored.")                  
 
 class DomainResourceText(models.Model): 
     _name = "hc.domain.resource.text"   
@@ -21,18 +36,27 @@ class DomainResourceContained(models.Model):
     _description = "Domain Resource Contained"          
     _inherit = ["hc.basic.association", "hc.resource"]
 
-    domain_resource_id = fields.Many2one(comodel_name="hc.res.domain.resource", string="Domain Resource", help="Domain Resource associated with this Domain Resource Contained.")                   
+    domain_resource_id = fields.Many2one(
+        comodel_name="hc.res.domain.resource", 
+        string="Domain Resource", 
+        help="Domain Resource associated with this Domain Resource Contained.")                   
 
 class DomainResourceExtension(models.Model):    
     _name = "hc.domain.resource.extension"  
     _description = "Domain Resource Extension"          
     _inherit = ["hc.basic.association", "hc.extension"]
 
-    domain_resource_id = fields.Many2one(comodel_name="hc.res.domain.resource", string="Domain Resource", help="Domain Resource associated with this Domain Resource Extension.")                   
+    domain_resource_id = fields.Many2one(
+        comodel_name="hc.res.domain.resource", 
+        string="Domain Resource", 
+        help="Domain Resource associated with this Domain Resource Extension.")                   
 
 class DomainResourceModifierExtension(models.Model):    
     _name = "hc.domain.resource.modifier.extension" 
     _description = "Domain Resource Modifier Extension"         
     _inherit = ["hc.basic.association", "hc.extension"]
 
-    domain_resource_id = fields.Many2one(comodel_name="hc.res.domain.resource", string="Domain Resource", help="Domain Resource associated with this Domain Resource Modifier Extension.")                  
+    domain_resource_id = fields.Many2one(
+        comodel_name="hc.res.domain.resource", 
+        string="Domain Resource", 
+        help="Domain Resource associated with this Domain Resource Modifier Extension.")                  
